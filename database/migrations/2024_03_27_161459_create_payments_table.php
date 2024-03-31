c036bf5bc7fea084f2d4e7b2deec029daa19c8de
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-03-31 17:31:20 +0630

    start project

---
+++ database/migrations/2024_03_27_161459_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('payment_method');
+            $table->string('payment_screenshot')->nullable();
+            $table->decimal('total_price', 10, 2);
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('paid_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
